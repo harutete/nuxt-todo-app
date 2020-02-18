4bb42783f2754109a551b1afb1d7ff603b42f8f6
harutete <ai.takeuchi.10@gmail.com>
2020-02-18 23:43:59 +0900

    datepickerを変更

--- components/Form.vue
+++ components/Form.vue
@@ -28,7 +28,27 @@
           cols="12"
           md="6"
         >
-          <v-date-picker v-model="picker"></v-date-picker>
+        <v-menu
+          ref="menu1"
+          v-model="menu1"
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+          max-width="290px"
+          min-width="290px"
+        >
+          <template v-slot:activator="{ on }">
+            <v-text-field
+              v-model="dateFormatted"
+              label="Date"
+              hint="MM/DD/YYYY"
+              persistent-hint
+              @blur="date = parseDate(dateFormatted)"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="date" no-title @input="menu = false"></v-date-picker>
+        </v-menu>
           <v-row>
             <v-col
               cols="12"
@@ -74,9 +94,36 @@ export default class FormComponent extends Vue {
   hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
   minutes = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]
   tagText = ''
+  menu = false
+  date = new Date().toISOString().substr(0, 10)
+  dateFormatted = this.formatDate(new Date().toISOString().substr(0, 10))
+
+  get watchDateFormatted () {
+    return this.formatDate(this.date)
+  }
 
   get setTagText (): string {
     return this.tagText === '' ? 'タグテキスト' : this.tagText
+  }
+
+  get computedDateFormatted () {
+    return this.formatDate(this.date)
+  }
+
+  private formatDate (date: string) {
+    if (!date) {
+      return null
+    }
+    const [year, month, day] = date.split('-')
+    return `${month}/${day}/${year}`
+  }
+
+  private parseDate (date) {
+    if (!date) {
+      return null
+    }
+    const [month, day, year] = date.split('/')
+    return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
   }
 }
 </script>
