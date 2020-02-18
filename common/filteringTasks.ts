284b1ce825da98d306a40342da1c53ebf842479f
harutete <ai.takeuchi.10@gmail.com>
2020-02-18 10:15:02 +0900

    型修正

--- common/filteringTasks.ts
+++ common/filteringTasks.ts
@@ -2,7 +2,7 @@ import { TaskDetail } from '~/types/'
 
 const filteringTasks = (tasks: TaskDetail[], terms: string): TaskDetail[] => {
   const filteredTasks = tasks
-    .filter((item: { [key: string]: any }) =>
+    .filter((item: TaskDetail) =>
       item.status === terms
     )
 
