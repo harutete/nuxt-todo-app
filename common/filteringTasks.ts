8b20cc02c665464d050eaa4bd214aa25b344d6c1
harutete <ai.takeuchi.10@gmail.com>
2020-03-07 02:42:48 +0900

    期限日の照準でタスクが並ぶように修正

--- common/filteringTasks.ts
+++ common/filteringTasks.ts
@@ -5,6 +5,9 @@ const filteringTasks = (tasks: TaskDetail[], terms: string): TaskDetail[] => {
     .filter((item: TaskDetail) =>
       item.status === terms
     )
+    .sort((a: any , b: any): number =>
+      a.ended_date.seconds * 1000 - b.ended_date.seconds * 1000 // 値が秒単位で返却されるためミリ秒に変換
+    )
 
   return filteredTasks
 }
