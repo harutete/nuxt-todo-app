9f99fd97fbb1a1cfa8d37608b323f632327c20c4
harutete <ai.takeuchi.10@gmail.com>
2020-03-10 01:12:47 +0900

    期限日をmsに変換した値を生成する

--- common/filteringTasks.ts
+++ common/filteringTasks.ts
@@ -2,11 +2,16 @@ import { TaskDetail } from '~/types/'
 
 const filteringTasks = (tasks: TaskDetail[], terms: string): TaskDetail[] => {
   const filteredTasks = tasks
+    .map((item: TaskDetail) => {
+      const period = item.ended_date.seconds
+      item.ended_ms = period * 1000
+      return item
+    })
     .filter((item: TaskDetail) =>
       item.status === terms
     )
     .sort((a: any , b: any): number =>
-      a.ended_date.seconds * 1000 - b.ended_date.seconds * 1000 // 値が秒単位で返却されるためミリ秒に変換
+      a.ended_ms - b.ended_ms
     )
 
   return filteredTasks
