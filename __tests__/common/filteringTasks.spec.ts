66e5bc2f89d42e361704a461e15d57b8e925f470
harutete <ai.takeuchi.10@gmail.com>
2020-03-22 23:47:23 +0900

    filteringTasksのテスト

---
+++ __tests__/common/filteringTasks.spec.ts
@@ -0,0 +1,15 @@
+import { TaskDetail } from '~/types'
+import filteringTasks from '~/common/filteringTasks'
+import stubTaskData from '~/test/stub/stubTaskData.json'
+
+const taskData: TaskDetail[] = stubTaskData
+describe('filteringTasks', () => {
+  it('ended_msが生成されている', () => {
+    console.log(taskData)
+    const filteredTasks: TaskDetail[] = filteringTasks(taskData, 'before')
+
+    filteredTasks.forEach((task) => {
+      expect(task.ended_ms).not.toBe('undefined')
+    })
+  })
+})
