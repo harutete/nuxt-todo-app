7d36798713d84b46362f8cb9e85144c77ffb50d8
harutete <ai.takeuchi.10@gmail.com>
2020-03-23 23:59:29 +0900

    console消す

--- __tests__/common/filteringTasks.spec.ts
+++ __tests__/common/filteringTasks.spec.ts
@@ -5,7 +5,6 @@ import stubTaskData from '~/__tests__/stub/stubTaskData.json'
 const taskData: TaskDetail[] = stubTaskData
 describe('filteringTasks', () => {
   it('ended_msが生成されている', () => {
-    console.log(taskData)
     const filteredTasks: TaskDetail[] = filteringTasks(taskData, 'before')
 
     filteredTasks.forEach((task) => {
